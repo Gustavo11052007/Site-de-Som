8c69e1293520f833efa18526d57607ac13a32771
Hikari <127319022+Gustavo11052007@users.noreply.github.com>
2023-11-29 11:16:24 -0300

    s

--- mains.js
+++ mains.js
@@ -1,6 +1,14 @@
 function tocarSomPom (){
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla tecla_pom').onclick=tocarSomPom;
+document.querySelector('.tecla tecla_pom').onclick = tocarSomPom;
 
-const listadeTeclas
+const listadeTeclas = document.querySelectorAll('.tecla')
+
+
+
+
+
+
+
+
