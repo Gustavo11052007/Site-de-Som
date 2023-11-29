b70ebe82613e2cf6d2a4bf79cb07710735cad183
Hikari <127319022+Gustavo11052007@users.noreply.github.com>
2023-11-29 11:22:16 -0300

    s

--- mains.js
+++ mains.js
@@ -5,8 +5,18 @@ document.querySelector('.tecla tecla_pom').onclick = tocarSomPom;
 
 const listadeTeclas = document.querySelectorAll('.tecla')
 
+let contador = 0;
+
+while (contador < listadeTeclas.length ){
+    const instrumento = listadeTeclas[contador].classList
+    console.log(instrumento[1])
+    listadeTeclas[contador].onclick = function (){
+
+    
 
 
+
+}
 
 
 
